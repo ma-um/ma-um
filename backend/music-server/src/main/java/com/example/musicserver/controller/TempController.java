0ca7c2a13ed523729991d549fae4bdf88b7e1ced
soulplay95 <soulplay95@gmail.com>
2021-11-03 06:09:58 +0900

    build: Add temporary code for deployment test to music-server

---
+++ backend/music-server/src/main/java/com/example/musicserver/controller/TempController.java
@@ -0,0 +1,24 @@
+package com.example.musicserver.controller;
+
+import com.example.musicserver.dto.BaseResponseBody;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/temp")
+@RestController
+public class TempController {
+
+    @GetMapping("/test")
+    public ResponseEntity<? extends BaseResponseBody> test() {
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "OK"));
+    }
+
+}
---
+++ backend/music-server/src/main/java/com/example/musicserver/dto/BaseResponseBody.java
@@ -0,0 +1,38 @@
+package com.example.musicserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Base Response Body
+ */
+@Getter
+@Setter
+//@ApiModel("Base Response Body")
+public class BaseResponseBody {
+
+//	@ApiModelProperty(value = "응답 메시지", example = "OK")
+	String message = null;
+//	@ApiModelProperty(value = "응답 코드", example = "200")
+	Integer statusCode = null;
+	
+	public BaseResponseBody() {}
+	
+	public BaseResponseBody(Integer statusCode){
+		this.statusCode = statusCode;
+	}
+	
+	public BaseResponseBody(Integer statusCode, String message){
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+	
+	public static BaseResponseBody of(Integer statusCode, String message) {
+		BaseResponseBody body = new BaseResponseBody();
+		body.message = message;
+		body.statusCode = statusCode;
+
+		return body;
+	}
+
+}
