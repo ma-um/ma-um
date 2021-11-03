da2d41b1c87a96a71b87e93990f93f6fef826f6d
soulplay95 <soulplay95@gmail.com>
2021-11-03 11:31:17 +0900

    build: Auth, diary, music server build, ssl test

---
+++ backend/diary-server/src/main/java/com/example/diaryserver/controller/TempController.java
@@ -0,0 +1,22 @@
+package com.example.diaryserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/temp")
+@RestController
+public class TempController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "다이어리";
+    }
+
+}
