83059d5f3a5d147513500ac46539a7f92bbc9833
cherrytomato1 <terec@naver.com>
2021-11-14 17:02:12 +0900

    #2 feat: querydsl 의존성 설정

--- backend/diary-server/src/main/java/com/spuit/maum/diaryserver/domain/tag/TagRepository.java
+++ backend/diary-server/src/main/java/com/spuit/maum/diaryserver/domain/tag/TagRepository.java
@@ -3,7 +3,7 @@ package com.spuit.maum.diaryserver.domain.tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Diary의 데이터를 관리하는 레포지토리. 도메인 애그리거트에만 의존.
+ * Tag의 데이터를 관리하는 레포지토리. 도메인 애그리거트에만 의존.
  *
  * @author cherrytomato1
  * @version 1.0.0
