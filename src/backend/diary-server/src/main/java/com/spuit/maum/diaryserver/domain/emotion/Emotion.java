c1a5630cb91bab55f2b27b22c43abe9c3b8d713c
cherrytomato1 <terec@naver.com>
2021-11-18 19:43:48 +0900

    #22 feat: 최고 감정 음수로 설정

--- src/backend/diary-server/src/main/java/com/spuit/maum/diaryserver/domain/emotion/Emotion.java
+++ src/backend/diary-server/src/main/java/com/spuit/maum/diaryserver/domain/emotion/Emotion.java
@@ -1,21 +1,89 @@
 package com.spuit.maum.diaryserver.domain.emotion;
 
+import java.lang.reflect.Field;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class Emotion {
-  Integer fear;
-  Integer surprise;
-  Integer anger;
-  Integer sadness;
-  Integer neutrality;
 
-  Integer happiness;
-  Integer disgust;
-  Integer pleasure;
-  Integer embarrassment;
-  Integer unrest;
-  Integer bruise;
+  private String topEmotion;
+
+  private final Integer fear;
+  private final Integer surprise;
+  private final Integer anger;
+  private final Integer sadness;
+  private final Integer neutrality;
+
+  private final Integer happiness;
+  private final Integer disgust;
+  private final Integer pleasure;
+  private final Integer embarrassment;
+  private final Integer unrest;
+  private final Integer bruise;
+
+  public Emotion setTopEmotionValue() {
+    Field[] fields = this.getClass().getDeclaredFields();
+    try {
+      for (Field f : fields) {
+        f.setAccessible(true);
+        if (f.getType() != Integer.class || !f.getName().equals(this.topEmotion)) {
+          continue;
+        }
+        int nextValue = (int) f.get(this);
+
+        f.set(this, -nextValue);
+        break;
+      }
+    } catch (IllegalAccessException ex) {
+      throw new RuntimeException();
+    }
+    return this;
+  }
+
+  public Emotion setTopEmotionName() {
+    Field[] fields = this.getClass().getDeclaredFields();
+    try {
+      for (Field f : fields) {
+        f.setAccessible(true);
+        if (f.getType() != Integer.class || (Integer) f.get(this) >= 0) {
+          continue;
+        }
+        this.topEmotion = f.getName();
+        break;
+      }
+    } catch (IllegalAccessException ex) {
+      throw new RuntimeException();
+    }
+    return this;
+  }
+
+  public Emotion setDefaultTopEmotion() {
+    Field[] fields = this.getClass().getDeclaredFields();
+    try {
+      int maxValue = 0;
+      String topEmotionName = "happiness";
+      for (Field f : fields) {
+        f.setAccessible(true);
+        if (f.getType() != Integer.class) {
+          continue;
+        }
+
+        Integer value = (Integer) f.get(this);
+        if (maxValue > value) {
+          continue;
+        }
+
+        maxValue = value;
+        topEmotionName = f.getName();
+      }
+      topEmotion = topEmotionName;
+      setTopEmotionValue();
+
+    } catch (IllegalAccessException ex) {
+      throw new RuntimeException();
+    }
+    return this;
+  }
 }
